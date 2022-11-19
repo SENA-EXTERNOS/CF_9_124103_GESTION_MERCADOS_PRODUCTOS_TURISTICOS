157de980a78b8c7d39e3f5defabf429e07d739e9
roberthdiiaz <50808254+roberthdiiaz@users.noreply.github.com>
2022-11-18 22:17:18 -0500

    vinculacion - video introduccion

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -18,7 +18,7 @@
     br
     figure
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/K759xlsij4k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
       figcaption VIDEO
     
 </template>
